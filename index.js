25419886801b9073c020b3bffee3d5040e64ff56
Tyler B Yeager <wow_d2@hotmail.com>
2021-10-13 18:10:41 -0700

    add initial readme questions

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const util = require('util');
 
-// const questions = [
-    
-// ]
+const questions = [
+    {
+        type= "input",
+        message: "What is your Github username?",
+        name: "username",
+    },
+    {
+        type: "input",
+        message: "What is your repo's name?",
+        name: "repo",
+    },
+    {
+        type: "input",
+        message: "What is your project's name/title?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "Describe your project",
+        name: "description",
+    },
+]
